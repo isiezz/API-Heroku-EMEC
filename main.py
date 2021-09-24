67ce02530febf336daeca2ab3304e88bf7621c69
Enderson Menezes (Mr. Enderson) <endersonster@gmail.com>
2021-09-24 19:50:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
-monki_brasill = open("mamacos.json")
-monki_brasil = monki_brasill.monki_numbers.monki_brasil
+monki_arquivo = open("mamacos.json")
+monki_geral = monki_arquivo.monki_numbers
 
 @app.route("/" , methods=['GET', 'POST'])
 def index():
   return render_template('index.html')
 
-@app.route('/monkey_number', methods=['GET', 'POST'])
+@app.route('/monkey_number', methods=['POST'])
 def monkey_number():
-  brasil=request.form['brasil'] 
+  pais=request.form['pais'] 
   return render_template('brasil.html', Brasil= monki_brasil)
 
 

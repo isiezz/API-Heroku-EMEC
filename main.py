5963ecfee36ab86e19981e61578d2dcf35ad3843
isiezz_ <83096990+isiezz@users.noreply.github.com>
2021-09-24 22:42:12 +0000

    adicionando 'monkes in brasil'

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
-
+monki_brasill = open("mamacos.json")
+monki_brasil = monki_brasill.monki_numbers.monki_brasil
 
 @app.route("/" , methods=['GET', 'POST'])
 def index():
@@ -9,7 +10,7 @@ def index():
 @app.route('/monkey_number', methods=['GET', 'POST'])
 def monkey_number():
   brasil=request.form['brasil'] 
-  return render_template('brasil.html', Brasil= 118)
+  return render_template('brasil.html', Brasil= monki_brasil)
 
 
 if __name__ == "__main__":
